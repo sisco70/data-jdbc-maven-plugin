dfe088538e737ab4424cb8d71468552050fcf704
Francesco Guarnieri <sisco70@users.noreply.github.com>
2026-03-01 11:26:47 +0100

    Added generationDate in comment, added maven license plugin

--- src/main/java/net/guarnie/maven/plugin/data/jdbc/GeneratorMojo.java
+++ src/main/java/net/guarnie/maven/plugin/data/jdbc/GeneratorMojo.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2026 Francesco Guarnieri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.guarnie.maven.plugin.data.jdbc;
 
 
@@ -23,6 +38,8 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 /**
@@ -33,6 +50,7 @@ import java.util.*;
 public class GeneratorMojo extends AbstractMojo {
     private static final Logger log = LoggerFactory.getLogger(GeneratorMojo.class);
 
+    private static final DateTimeFormatter DATE_TIME_ISO_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     private static final String OFFSETDATETIME_CLASS = "java.time.OffsetDateTime";
     private static final String INSTANT_CLASS = "java.time.Instant";
 
@@ -182,6 +200,7 @@ public class GeneratorMojo extends AbstractMojo {
 
         // Prepare context for Handlebars template
         Map<String, Object> context = Map.of(
+                "generationDate", LocalDateTime.now().format(DATE_TIME_ISO_FORMAT),
                 "packageName", packageName,
                 "className", javaClassName,
                 "dbTableName", tableName,
